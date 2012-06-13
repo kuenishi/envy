8f8b3305c4fd899d1a4349d8dd8d8e784f7dd9e5
UENISHI Kota <kuenishi+github@gmail.com>
2012-06-13 22:20:43 +0900

    basic installer

--- local/bin/envy.py
+++ local/bin/envy.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 
-import sys
+import sys, os
+import json
+import subprocess
 
 '''
 envy_install http://ham.egg.com/spam-0.1.1.tar.gz
@@ -22,10 +24,51 @@ envy_install git://github.com/...
 ->
 '''
 
+def get_basedir(relative = None):
+    base = os.environ['ENVY_HOME']
+    if relative is None: return base
+    else:                return '%s/%s' % (base, relative)
+
 def help():
     print('help: ... ')
 
+def fetch(url):
+    srcdir = get_basedir('local/src')
+    try: os.mkdir(srcdir)
+    except: pass
+    subprocess.call(['wget', url, '--directory-prefix=%s'%srcdir])
+    filename = url.split('/')[-1]
+    return filename
 
+def deflate(srcdir, tarball):
+    subprocess.call(['tar', 'xzf', '%s/%s' % (srcdir, tarball),
+                     '-C', srcdir])
+    if tarball[-6:] == 'tar.gz':
+        basename = tarball[0:-7]
+        return '%s/%s' % (srcdir, basename)
+    elif tarball[-4:] == '.tgz':
+        basename = tarball[0:-5]
+        return '%s/%s' % (srcdir, basename)
+
+def build(srcdir):
+    os.chdir(srcdir)
+    # run configure.py
+    subprocess.call(['configure.py'])
+    # run make && make install
+    subprocess.call(['make'])
+    return subprocess.call(['make', 'install'])
+
+def install(name):
+    print('installing %s' % name)
+    resource_file = '%s/local/share/packages.json' % os.environ['ENVY_HOME']
+    #print(resource_file)
+    v = json.load(open(resource_file))
+    url = v[name]
+    print(url)
+    tarball = fetch(url)
+    srcdir = get_basedir('local/src')
+    srcdir  = deflate(srcdir, tarball)
+    return build(srcdir)
 
 if __name__ == '__main__':
     cmd = None
@@ -33,12 +76,13 @@ if __name__ == '__main__':
         cmd = 'help'
     else:
         cmd = sys.argv[1]
-    print(len(sys.argv))
+
     if cmd == 'help':
         help()
     elif cmd == 'install' and len(sys.argv) > 2:
         # read local/share/envy_resource.json
         # look for req
-        print('envy install <%s>' % sys.argv[2])
+        install(sys.argv[2])
+
     else:
         print('error, assert!')
